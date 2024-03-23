c45deb95b37692542d5b1275ed15abb78a03d844
joamik <joamik@users.noreply.github.com>
2024-03-23 20:16:03 +0100

    Refactor - use static imports

--- src/main/java/io/github/joamik/cinema/reservation/domain/SeatsCreator.java
+++ src/main/java/io/github/joamik/cinema/reservation/domain/SeatsCreator.java
@@ -2,15 +2,16 @@ package io.github.joamik.cinema.reservation.domain;
 
 import java.math.BigDecimal;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toUnmodifiableMap;
 
 public class SeatsCreator {
 
     public static Map<SeatNumber, Seat> createSeats(BigDecimal price) {
         return IntStream.rangeClosed(1, 10)
                 .mapToObj(number -> new Seat(new SeatNumber(number), SeatStatus.AVAILABLE, price))
-                .collect(Collectors.toUnmodifiableMap(Seat::number, Function.identity()));
+                .collect(toUnmodifiableMap(Seat::number, identity()));
     }
 }
